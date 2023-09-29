a0e2c0460fb7eebe88b4d0016e66c9c16ed7cd26
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2023-09-29 18:12:06 +0700

    Update Structs.sol

--- Structs.sol
+++ Structs.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
 
+library Structs {
+    struct MintInput {
+        uint32 sourceChain;
+        address token;
+        uint256 amount;
+        address receiver;
+        string wrappedTokenName;
+        string wrappedTokenSymbol;
+        bytes txHash;
+        bytes txSigned;
+        uint256 timestamp;
+    }
+
+    struct ReleaseInput {
+        uint32 sourceChain;
+        address token;
+        uint256 amount;
+        address receiver;
+        bytes txHash;
+        bytes txSigned;
+        uint256 timestamp;
+    }
+
+    struct WrappedToken {
+        string name;
+        string symbol;
+        uint8 decimals;
+        address wrappedToken;
+        address token;
+        uint32 sourceChain;
+    }
+}
